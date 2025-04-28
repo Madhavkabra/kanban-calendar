a1431abeef58bad33f078213bc29618f69f5b20d
Madhavkabra <madhav.kabra1100@gmail.com>
2025-04-28 19:38:06 +0530

    fix: updated duration to make animation a bit faster

--- components/CalendarModal/index.tsx
+++ components/CalendarModal/index.tsx
@@ -29,7 +29,7 @@ export default function CalendarModal({
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
-            transition={{ duration: 0.6 }}
+            transition={{ duration: 0.1 }}
           />
         )}
 
